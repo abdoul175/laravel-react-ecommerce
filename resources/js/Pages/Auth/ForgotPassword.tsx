dfaa00f2a022fabc3028942e518f9fdc9adb47d0
Sama Abdoul Sabour <abdoulsaboursama51@gmail.com>
2025-01-05 19:11:08 +0000

    Create and user new Navbar using daisyUI

--- resources/js/Pages/Auth/ForgotPassword.tsx
+++ resources/js/Pages/Auth/ForgotPassword.tsx
@@ -1,6 +1,7 @@
 import InputError from "@/Components/Core/InputError";
 import PrimaryButton from "@/Components/Core/PrimaryButton";
 import TextInput from "@/Components/Core/TextInput";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import GuestLayout from "@/Layouts/GuestLayout";
 import { Head, useForm } from "@inertiajs/react";
 import { FormEventHandler } from "react";
@@ -17,40 +18,54 @@ export default function ForgotPassword({ status }: { status?: string }) {
     };
 
     return (
-        <GuestLayout>
+        <AuthenticatedLayout>
             <Head title="Forgot Password" />
 
-            <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                Forgot your password? No problem. Just let us know your email
-                address and we will email you a password reset link that will
-                allow you to choose a new one.
+            <div className="p-8">
+
+                <div className="card bg-white shadow max-w-[420px] mx-auto">
+
+                    <div className="card-body">
+
+                        <div className="mb-4 text-sm text-gray-600 dark:text-gray-400" >
+                            Forgot your password ? No problem.Just let us know your email
+                            address and we will email you a password reset link that will
+                            allow you to choose a new one.
+                        </div>
+
+                        {
+                            status && (
+                                <div className="mb-4 text-sm font-medium text-green-600 dark:text-green-400" >
+                                    {status}
+                                </div>
+                            )
+                        }
+
+                        <form onSubmit={submit}>
+                            <TextInput
+                                id="email"
+                                type="email"
+                                name="email"
+                                value={data.email}
+                                className="mt-1 block w-full"
+                                isFocused={true}
+                                onChange={(e) => setData("email", e.target.value)
+                                }
+                            />
+
+                            < InputError message={errors.email} className="mt-2" />
+
+                            <div className="mt-4 flex items-center justify-end" >
+                                <PrimaryButton className="ms-4" disabled={processing} >
+                                    Email Password Reset Link
+                                </PrimaryButton>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
             </div>
 
-            {status && (
-                <div className="mb-4 text-sm font-medium text-green-600 dark:text-green-400">
-                    {status}
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <TextInput
-                    id="email"
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    className="mt-1 block w-full"
-                    isFocused={true}
-                    onChange={(e) => setData("email", e.target.value)}
-                />
-
-                <InputError message={errors.email} className="mt-2" />
-
-                <div className="mt-4 flex items-center justify-end">
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Email Password Reset Link
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+        </AuthenticatedLayout>
     );
 }
